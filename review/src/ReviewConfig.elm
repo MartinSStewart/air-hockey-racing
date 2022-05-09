a4840dbe2ab43250efe4c3cf3444b9b170d3c5aa
Martin Stewart <MartinSStewart@gmail.com>
2022-05-09 14:40:13 +0200

    Update elm-review

--- review/src/ReviewConfig.elm
+++ review/src/ReviewConfig.elm
@@ -11,10 +11,26 @@ when inside the directory containing this file.
 
 -}
 
-import NoUnused.Dependencies
-import Review.Rule exposing (Rule)
 
+import NoUnused.CustomTypeConstructorArgs
+import NoUnused.CustomTypeConstructors
+import NoUnused.Dependencies
+import NoUnused.Exports
+import NoUnused.Modules
+import NoUnused.Parameters
+import NoUnused.Patterns
+import NoUnused.Variables
+import Review.Rule exposing (Rule)
 
 config : List Rule
 config =
-    [ NoUnused.Dependencies.rule ]
+    [ NoUnused.CustomTypeConstructors.rule []
+    , NoUnused.CustomTypeConstructorArgs.rule
+    , NoUnused.Dependencies.rule
+    , NoUnused.Exports.rule
+    , NoUnused.Modules.rule
+    , NoUnused.Parameters.rule
+    , NoUnused.Patterns.rule
+    , NoUnused.Variables.rule
+    ]
+        |> List.map (Review.Rule.ignoreErrorsForDirectories [ "src/Evergreen" ])
