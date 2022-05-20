d15199cd9d3e92f454dd2d8e6340f397784b4685
Martin Stewart <MartinSStewart@gmail.com>
2022-05-20 23:57:17 +0200

    Preparing for v1

---
+++ src/Evergreen/V1/Id.elm
@@ -0,0 +1,5 @@
+module Evergreen.V1.Id exposing (..)
+
+
+type Id idType
+    = Id Int
---
+++ src/Evergreen/V1/Sounds.elm
@@ -0,0 +1,8 @@
+module Evergreen.V1.Sounds exposing (..)
+
+import Evergreen.V1.Audio
+
+
+type alias Sounds =
+    { buttonPress : Evergreen.V1.Audio.Source
+    }
