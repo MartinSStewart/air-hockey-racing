223f223a07f0d0d76d289fc2462efed61960fb4e
Martin Stewart <MartinSStewart@gmail.com>
2022-05-21 11:08:33 +0200

    Preparing for v3

---
+++ src/Evergreen/V3/Keyboard/Arrows.elm
@@ -0,0 +1,13 @@
+module Evergreen.V3.Keyboard.Arrows exposing (..)
+
+
+type Direction
+    = North
+    | NorthEast
+    | East
+    | SouthEast
+    | South
+    | SouthWest
+    | West
+    | NorthWest
+    | NoDirection
